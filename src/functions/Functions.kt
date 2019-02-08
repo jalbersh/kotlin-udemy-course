87a2e50fb74863fff490a9ff59955a440795f43e
James W Albersheim <macbookpro2018@Jamess-MacBook-Pro-2018.local>
2019-02-08 16:55:30 -0700

    more functions

--- src/functions/Functions.kt
+++ src/functions/Functions.kt
@@ -5,6 +5,23 @@ import java.util.*
 fun getCurrentDate(): Date {
     return Date()
 }
+fun reverse(list: List<Int>):List<Int> {
+    var revList = arrayListOf<Int>()
+    for (i in 0..list.size-1) {
+        revList.add(list.get(list.size-1-i))
+    }
+    return revList
+}
+fun reverse2(list: List<Int>):List<Int> {
+    var revList = arrayListOf<Int>()
+    for (i in list.size-1 downTo 0) {
+        revList.add(list.get(i))
+    }
+    return revList
+}
 fun main(args: Array<String>) {
     println(getCurrentDate())
+    println()
+    println(reverse(listOf(1,3,5,6,8,9)))
+    println(reverse2(listOf(1,3,5,6,8,9)))
 }
